f26a71d829bcde981ae14420ce71176ce556e1bc
Tariq ali <76946087+Tariq-the-Coder@users.noreply.github.com>
2023-03-14 18:15:28 +0530

    Update trade.js

--- trade.js
+++ trade.js
@@ -15,14 +15,19 @@ const serverName = process.env.PASSWORD;
 const password = process.env.SERVER_NAME;
 const API_URL = process.env.META_API_SERVER_URL;
 const api = new MetaApi(token);
-let account, connection;
 
-// Add test MetaTrader account and deploy it
-async function deployAccount() {
+
+app.get('/', async(req, res) => {
+    res.send("Server Ready")
+});
+
+app.post('/trade', async(req, res) => {
+    const { actionType, symbol, volume, openPrice, stopLoss, takeProfit } = req.body;
+    req.setTimeout(60000); // Set timeout to 60 seconds (in milliseconds)
     try {
-        // Check if account already exists
+        // Add test MetaTrader account
         let accounts = await api.metatraderAccountApi.getAccounts();
-        account = accounts.find(a => a.login === login && a.type.startsWith('cloud'));
+        let account = accounts.find(a => a.login === login && a.type.startsWith('cloud'));
         if (!account) {
             console.log('Adding MT4 account to MetaApi');
             account = await api.metatraderAccountApi.createAccount({
@@ -32,6 +37,7 @@ async function deployAccount() {
                 password: password,
                 server: serverName,
                 platform: 'mt4',
+
             });
         } else {
             console.log('MT4 account already added to MetaApi');
@@ -44,33 +50,12 @@ async function deployAccount() {
         await account.waitConnected();
 
         // connect to MetaApi API
-        connection = account.getRPCConnection();
+        let connection = account.getRPCConnection();
         await connection.connect();
 
         // wait until terminal state synchronized to the local state
         console.log('Waiting for SDK to synchronize to terminal state (may take some time depending on your history size)');
         await connection.waitSynchronized();
-
-        console.log('Account deployed and connected');
-    } catch (err) {
-        console.error('Error deploying account', err);
-    }
-}
-
-deployAccount();
-
-app.get('/', async(req, res) => {
-    res.send("Server Ready")
-});
-
-app.post('/trade', async(req, res) => {
-    const { actionType, symbol, volume, openPrice, stopLoss, takeProfit } = req.body;
-    try {
-        if (!account || !connection) {
-            console.error('Account or connection not available');
-            res.status(500).send('Internal server error');
-            return;
-        }
 
         // Trade Place 
         const order = {
